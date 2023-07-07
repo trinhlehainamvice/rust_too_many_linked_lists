bf7917fb6101ae020e94b0502df469e664d5de0c
trinhlehainamvice <125254137+trinhlehainamvice@users.noreply.github.com>
2023-07-07 17:11:48 +0900

    Add reference iter to learn about lifetime and how it work with generic.

--- src/an_ok_stack/mod.rs
+++ src/an_ok_stack/mod.rs
@@ -17,6 +17,35 @@ pub struct OkStack<T> {
     head: OptionList<T>,
 }
 
+// Consume Iterator, mean we can't use OkStack after convert into Iterator
+// Because IntoIter will own OkStack instance.
+pub struct IntoIter<T>(OkStack<T>);
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+    // Prefer &T rather than &Box<T>
+    // Because we can use deref() on &Box<T> to get &T
+    // Reference: https://rust-lang.github.io/rust-clippy/master/index.html#/borrowed_box
+    // next: Option<&'a Box<Node<T>>>
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_deref();
+            &node.val
+        })
+    }
+}
+
 impl<T> OkStack<T> {
     pub fn new() -> Self {
         Self { head: None }
@@ -56,6 +85,20 @@ impl<T> OkStack<T> {
     pub fn peek_mut(&mut self) -> Option<&mut T> {
         self.head.as_mut().map(|node| &mut node.val)
     }
+
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+
+    // pub fn iter<'a>(&'a self) -> Iter<'a, T>
+    // lifetime elision will automatically detect how to match lifetime in certain cases
+    // so we don't need to write lifetime parameter like above case to more readable
+    // Reference: https://rust-lang.github.io/rust-clippy/master/index.html#/needless_lifetimes
+    pub fn iter(&self) -> Iter<T> {
+        Iter {
+            next: self.head.as_deref(),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -73,5 +116,25 @@ mod tests {
         assert_eq!(list.pop(), Some(3));
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), None);
+
+        list.push(0);
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
+        assert_eq!(iter.next(), Some(&0));
+        assert_eq!(iter.next(), None);
+        println!("{:?}", list);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), Some(0));
+        assert_eq!(iter.next(), None);
     }
 }
