608f6c43dac1b2eb10040fa30b83766d36e36a22
trinhlehainamvice <125254137+trinhlehainamvice@users.noreply.github.com>
2023-07-08 16:55:29 +0900

    Add IntoIter for a bad stack, can't find a way to implement Iter or IterMut because of Ref and RefCell borrow.

--- src/a_bad_safe_deque/mod.rs
+++ src/a_bad_safe_deque/mod.rs
@@ -1,4 +1,5 @@
 use std::cell::{Ref, RefCell, RefMut};
+use std::ops::Deref;
 use std::rc::Rc;
 
 struct Node<T> {
@@ -135,6 +136,38 @@ impl<T> Drop for List<T> {
     }
 }
 
+struct IntoIter<T>(List<T>);
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop_front()
+    }
+}
+
+impl<T> DoubleEndedIterator for IntoIter<T> {
+    fn next_back(&mut self) -> Option<T> {
+        self.0.pop_back()
+    }
+}
+
+impl<T> List<T> {
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+/* struct Iter<T>(Option<Rc<RefCell<Node<T>>>>);
+   With Rc version, we can't define lifetime of Iter
+   So we can't get Ref or RefMut of RefCell if we can't define lifetime
+*/
+
+// TODO: research
+/* struct Iter<'a, T>(Option<Ref<'a, Node<T>>);
+   With Ref version, even though we can define lifetime
+   We still can't handle borrow properly
+*/
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -157,5 +190,17 @@ mod tests {
         assert_eq!(*list.front().unwrap(), 1);
         assert_eq!(list.pop_front(), Some(1));
         assert_eq!(list.pop_front(), None);
+    }
+
+    #[test]
+    fn test_iter() {
+        let mut list = List::new();
+        list.push_front(1);
+        list.push_front(2);
+        list.push_front(3);
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next_back(), Some(1));
+        assert_eq!(iter.next(), Some(2));
     }
 }
