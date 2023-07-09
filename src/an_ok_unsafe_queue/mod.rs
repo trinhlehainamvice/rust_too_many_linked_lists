3cdaf3f1f7ce3ee05cde3af8ec9d42600e8b6a52
trinhlehainamvice <125254137+trinhlehainamvice@users.noreply.github.com>
2023-07-09 20:19:25 +0900

    Learning Rust Borrow Stack.

--- src/an_ok_unsafe_queue/mod.rs
+++ src/an_ok_unsafe_queue/mod.rs
@@ -1,2 +1,3 @@
 mod first;      // Safe but failed
 mod second;     // Unsafe
+mod stack_borrowed;
---
+++ src/an_ok_unsafe_queue/stack_borrowed.rs
@@ -0,0 +1,145 @@
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test1() {
+        let mut a = 3;
+        // Push first reference to a to borrowed stack
+        let ref1 = &mut a;
+        // Push second reference to a to borrowed stack
+        // Because Rust doesn't allow to explicitly reference to a more than once at a time
+        // So we need to reference a through deference ref1
+        // This way we also can keep track of stack, this just like push a element to a stack
+        let ref2 = &mut *ref1;
+
+        // Use raw pointer to reborrow
+        let ptr: *mut _ = &mut *ref2;
+
+        // Borrowed stack: ref2 -> ref1
+        // Can't use ref1 before ref2
+        // Rust will pop stack until found a reference that a line of code using
+        // In this case, ref1 is used, so ref2 is popped
+        // Mean try to use ref2 after ref1 will cause panic
+        // *ref1 += 1;
+
+        // *ref2 += 1;
+
+        unsafe {
+            *ptr += 1;
+        }
+
+        *ref2 += 1;
+        println!("{}", *ref1);
+        assert_eq!(*ref1, 5);
+
+        *ref1 += 1;
+    }
+
+    #[test]
+    fn test2() {
+        unsafe {
+            let mut a = 3;
+            let ref1 = &mut a;
+            // ptr1 is a raw pointer, not a reference
+            let ptr1 = ref1 as *mut _;
+            let ref2 = &mut *ptr1;
+            // ptr2 is a raw pointer, not a reference
+            let ptr2 = ref2 as *mut _;
+
+            // In miri perspective Borrow Stack should be like below order:
+            // ptr2 -> ref2 -> ptr1 -> ref1
+            // *mut -> &mut -> *mut -> &mut
+
+            // Even we access in violated order of Borrow Stack
+            // Rust still build program if we put these inside unsafe block
+            // *ref1 += 1;
+            // *ref2 += 1;
+            // *ptr1 += 1;
+            // *ptr2 += 1;
+            //
+
+            // So if we access in right order of Borrow Stack, miri test will be passed
+            // ptr2 -> ref2 -> ptr1 -> ref1
+            *ptr2 += 1;
+            // ref2 -> ptr1 -> ref1
+            *ref2 += 1;
+            // ptr1 -> ref1
+            *ptr1 += 1;
+            // ref1
+            *ref1 += 1;
+
+            assert_eq!(a, 7)
+        }
+    }
+
+    #[test]
+    fn test3() {
+        unsafe {
+            let mut data = [0; 10];
+            let ref1_at_0 = &mut data[0];
+            let ptr1_at_0 = ref1_at_0 as *mut i32;
+            let ref2_at_0 = &mut *ptr1_at_0;
+            let ptr2_at_0 = ref2_at_0 as *mut i32;
+            // raw_ptr.add() or sub() is offset a size of type that this pointer point to (i32 in this case)
+            // offset zero size so it still pointer at the same address
+            let ptr3_at_0 = ptr2_at_0.add(0);
+            // add 1 i32 size and sub 1 i32 -> no offset -> same as add(0)
+            let ptr4_at_0 = ptr3_at_0.add(1).sub(1);
+
+            // Borrow Stack:
+            // ptr4_at_0 -> ptr3_at_0 -> ptr2_at_0 -> ref2_at_0 -> ptr1_at_0 -> ref1_at_0
+            // Below is valid Borrow Stack access order
+            *ptr2_at_0 += 4;
+            *ptr3_at_0 += 4;
+            *ptr4_at_0 += 4;
+            *ref2_at_0 += 4;
+            *ptr1_at_0 += 1;
+            *ref1_at_0 += 3;
+            assert_eq!(data[0], 20);
+        }
+    }
+
+    #[test]
+    fn test4() {
+        unsafe {
+            let mut data = [0; 10];
+            let (slice1, slice2) = data.split_at_mut(1);
+            let ref1_at_0 = &mut slice1[0];
+            let ref1_at_1 = &mut slice2[0];
+            let ptr1_at_0 = ref1_at_0 as *mut i32;
+            let ptr1_at_1 = ref1_at_1 as *mut i32;
+
+            *ptr1_at_0 = 10;
+            *ref1_at_0 += 10;
+
+            *ptr1_at_1 = 20;
+            *ref1_at_1 += 20;
+
+            assert_eq!(data[0], 20);
+            assert_eq!(data[1], 40);
+        }
+    }
+
+    #[test]
+    fn test5() {
+        unsafe {
+            let mut data = [0; 10];
+            let slice_all = &mut data[..];
+            let ptr_all = slice_all.as_mut_ptr();
+            let ptr1_at_0 = ptr_all.add(0);
+            let ptr1_at_1 = ptr_all.add(1);
+            let ref2_at_0 = &mut *ptr1_at_0;
+
+            *ref2_at_0 += 10;
+
+            for i in 0..slice_all.len() {
+                *ptr1_at_0.add(i) += 30;
+            }
+
+            *ptr1_at_1 = 20;
+
+            assert_eq!(data[0], 40);
+        }
+    }
+}
