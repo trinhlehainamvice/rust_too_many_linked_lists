3a6396f2f1dee328173c2ee64a5f7e7a4b285c93
trinhlehainamvice <125254137+trinhlehainamvice@users.noreply.github.com>
2023-07-10 20:11:19 +0900

    Add necessary trait for our production deque.

--- src/a_production_unsafe_deque/first.rs
+++ src/a_production_unsafe_deque/first.rs
@@ -1,3 +1,5 @@
+use std::fmt::Debug;
+use std::hash::{Hash, Hasher};
 use std::marker::PhantomData;
 use std::ptr::NonNull;
 
@@ -7,21 +9,29 @@ struct Node<T> {
     prev: Option<NonNull<Node<T>>>,
 }
 
-struct List<T> {
+struct LinkedList<T> {
     head: Option<NonNull<Node<T>>>,
     tail: Option<NonNull<Node<T>>>,
     len: usize,
-    _marker: PhantomData<T>,
+    _phantom: PhantomData<T>,
 }
 
-impl<T> List<T> {
+impl<T> LinkedList<T> {
     pub fn new() -> Self {
         Self {
             head: None,
             tail: None,
             len: 0,
-            _marker: PhantomData,
+            _phantom: PhantomData,
         }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.len == 0
+    }
+
+    pub fn clear(&mut self) {
+        while self.pop_front().is_some() {}
     }
 
     pub fn push_front(&mut self, val: T) {
@@ -129,13 +139,13 @@ impl<T> List<T> {
     }
 }
 
-impl<T> Drop for List<T> {
+impl<T> Drop for LinkedList<T> {
     fn drop(&mut self) {
         while self.pop_front().is_some() {}
     }
 }
 
-struct IntoIter<T>(List<T>);
+struct IntoIter<T>(LinkedList<T>);
 
 impl<T> Iterator for IntoIter<T> {
     type Item = T;
@@ -163,21 +173,23 @@ struct Iter<'a, T> {
     head: Option<NonNull<Node<T>>>,
     tail: Option<NonNull<Node<T>>>,
     len: usize,
-    _marker: PhantomData<&'a T>,
+    _phantom: PhantomData<&'a T>,
 }
 
 impl<'a, T> Iterator for Iter<'a, T> {
     type Item = &'a T;
     fn next(&mut self) -> Option<Self::Item> {
-        self.head.map(|node| {
-            if self.len > 0 {
+        if self.len > 0 {
+            self.head.map(|node| {
                 self.len -= 1;
-            }
-            unsafe {
-                self.head = (*node.as_ptr()).next;
-                &(*node.as_ptr()).val
-            }
-        })
+                unsafe {
+                    self.head = (*node.as_ptr()).next;
+                    &(*node.as_ptr()).val
+                }
+            })
+        } else {
+            None
+        }
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
@@ -187,15 +199,17 @@ impl<'a, T> Iterator for Iter<'a, T> {
 
 impl<'a, T> DoubleEndedIterator for Iter<'a, T> {
     fn next_back(&mut self) -> Option<Self::Item> {
-        self.tail.map(|node| {
-            if self.len > 0 {
+        if self.len > 0 {
+            self.tail.map(|node| {
                 self.len -= 1;
-            }
-            unsafe {
-                self.tail = (*node.as_ptr()).prev;
-                &(*node.as_ptr()).val
-            }
-        })
+                unsafe {
+                    self.tail = (*node.as_ptr()).prev;
+                    &(*node.as_ptr()).val
+                }
+            })
+        } else {
+            None
+        }
     }
 }
 
@@ -209,21 +223,23 @@ struct IterMut<'a, T> {
     head: Option<NonNull<Node<T>>>,
     tail: Option<NonNull<Node<T>>>,
     len: usize,
-    _marker: PhantomData<&'a mut T>,
+    _phantom: PhantomData<&'a mut T>,
 }
 
 impl<'a, T> Iterator for IterMut<'a, T> {
     type Item = &'a mut T;
     fn next(&mut self) -> Option<Self::Item> {
-        self.head.map(|node| {
-            if self.len > 0 {
+        if self.len > 0 {
+            self.head.map(|node| {
                 self.len -= 1;
-            }
-            unsafe {
-                self.head = (*node.as_ptr()).next;
-                &mut (*node.as_ptr()).val
-            }
-        })
+                unsafe {
+                    self.head = (*node.as_ptr()).next;
+                    &mut (*node.as_ptr()).val
+                }
+            })
+        } else {
+            None
+        }
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
@@ -251,7 +267,7 @@ impl<'a, T> ExactSizeIterator for IterMut<'a, T> {
     }
 }
 
-impl<T> List<T> {
+impl<T> LinkedList<T> {
     pub fn into_iter(self) -> IntoIter<T> {
         IntoIter(self)
     }
@@ -261,7 +277,7 @@ impl<T> List<T> {
             head: self.head,
             tail: self.tail,
             len: self.len,
-            _marker: PhantomData,
+            _phantom: PhantomData,
         }
     }
 
@@ -270,14 +286,14 @@ impl<T> List<T> {
             head: self.head,
             tail: self.tail,
             len: self.len,
-            _marker: PhantomData,
+            _phantom: PhantomData,
         }
     }
 }
 
 // IntoIterator auto deduced a List to a iterator
 // for _ in list <=> for _ in list.into_iter()
-impl<T> IntoIterator for List<T> {
+impl<T> IntoIterator for LinkedList<T> {
     type Item = T;
     type IntoIter = IntoIter<T>;
 
@@ -287,7 +303,7 @@ impl<T> IntoIterator for List<T> {
 }
 
 // for _ in &list <=> for _ in list.iter()
-impl<'a, T> IntoIterator for &'a List<T> {
+impl<'a, T> IntoIterator for &'a LinkedList<T> {
     type Item = &'a T;
     type IntoIter = Iter<'a, T>;
 
@@ -297,12 +313,81 @@ impl<'a, T> IntoIterator for &'a List<T> {
 }
 
 // for _ in &mut list <=> for _ in list.iter_mut()
-impl<'a, T> IntoIterator for &'a mut List<T> {
+impl<'a, T> IntoIterator for &'a mut LinkedList<T> {
     type Item = &'a mut T;
     type IntoIter = IterMut<'a, T>;
 
     fn into_iter(self) -> Self::IntoIter {
         self.iter_mut()
+    }
+}
+
+// To put our collection to production, we need to implement below traits
+// [Default, Clone, Extend, FromIterator, Debug, PartialEq, Eq, PartialOrd, Ord, Hash]
+impl<T> Default for LinkedList<T> {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<T: Clone> Clone for LinkedList<T> {
+    fn clone(&self) -> Self {
+        let mut new_list = LinkedList::new();
+        for item in self {
+            new_list.push_back(item.clone());
+        }
+        new_list
+    }
+}
+
+impl<T> Extend<T> for LinkedList<T> {
+    fn extend<IntoIter: IntoIterator<Item = T>>(&mut self, iter: IntoIter) {
+        for item in iter {
+            self.push_back(item);
+        }
+    }
+}
+
+impl<T> FromIterator<T> for LinkedList<T> {
+    fn from_iter<IntoIter: IntoIterator<Item = T>>(iter: IntoIter) -> Self {
+        let mut new_list = LinkedList::new();
+        new_list.extend(iter);
+        new_list
+    }
+}
+
+impl<T: Debug> Debug for LinkedList<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_list().entries(self).finish()
+    }
+}
+
+impl<T: PartialEq> PartialEq for LinkedList<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.len() == other.len() && self.iter().zip(other.iter()).all(|(a, b)| a == b)
+    }
+}
+
+impl<T: Eq> Eq for LinkedList<T> {}
+
+impl<T: PartialOrd> PartialOrd for LinkedList<T> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.iter().partial_cmp(other.iter())
+    }
+}
+
+impl<T: Ord> Ord for LinkedList<T> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.iter().cmp(other.iter())
+    }
+}
+
+impl<T: Hash> Hash for LinkedList<T> {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.len().hash(state);
+        for item in self {
+            item.hash(state);
+        }
     }
 }
 //
@@ -315,7 +400,7 @@ mod tests {
     fn test() {
         println!("test a_production_unsafe_deque::first::tests::test");
 
-        let mut list = List::new();
+        let mut list = LinkedList::new();
         list.push_front(1);
         list.push_front(2);
         list.push_front(3);
